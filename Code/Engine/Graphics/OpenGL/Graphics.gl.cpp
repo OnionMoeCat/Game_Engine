6cb4e720f53deb7726b2f8e8ec1d29abac6551bc
Yuchen Zhang <u0950711@utah.edu>
2015-09-06 21:20:43 -0600

    Assignment 02

--- Code/Engine/Graphics/OpenGL/Graphics.gl.cpp
+++ Code/Engine/Graphics/OpenGL/Graphics.gl.cpp
@@ -156,8 +156,10 @@ void eae6320::Graphics::Render()
 			const GLenum mode = GL_TRIANGLES;
 			// It's possible to start rendering primitives in the middle of the stream
 			const GLint indexOfFirstVertexToRender = 0;
-			// We are drawing a single triangle
-			const GLsizei vertexCountToRender = 3;
+			// We are drawing two triangles
+			const unsigned int numTriangles = 2;
+			const unsigned int verticesPerTriangle = 3;
+			const GLsizei vertexCountToRender = numTriangles * verticesPerTriangle;
 			glDrawArrays( mode, indexOfFirstVertexToRender, vertexCountToRender );
 			assert( glGetError() == GL_NO_ERROR );
 		}
@@ -483,9 +485,10 @@ namespace
 		}
 		// Assign the data to the buffer
 		{
-			// We are drawing a single triangle
+			// We are drawing two triangles
+			const unsigned int numTriangles = 2;
 			const unsigned int verticesPerTriangle = 3;
-			sVertex vertexData[verticesPerTriangle];
+			sVertex vertexData[numTriangles * verticesPerTriangle];
 			// Fill in the data for the triangle
 			{
 				vertexData[0].x = 0.0f;
@@ -496,8 +499,17 @@ namespace
 
 				vertexData[2].x = 1.0f;
 				vertexData[2].y = 1.0f;
+
+				vertexData[3].x = 0.0f;
+				vertexData[3].y = 0.0f;
+
+				vertexData[4].x = 1.0f;
+				vertexData[4].y = 1.0f;
+
+				vertexData[5].x = 0.0f;
+				vertexData[5].y = 1.0f;
 			}
-			glBufferData( GL_ARRAY_BUFFER, sizeof( sVertex ) * verticesPerTriangle, reinterpret_cast<GLvoid*>( vertexData ),
+			glBufferData( GL_ARRAY_BUFFER, sizeof( sVertex ) * numTriangles * verticesPerTriangle, reinterpret_cast<GLvoid*>( vertexData ),
 				// Our code will only ever write to the buffer
 				GL_STATIC_DRAW );
 			const GLenum errorCode = glGetError();
